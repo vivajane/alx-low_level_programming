b5c6f031806b8efa84ab31a296e853a4c6cd4618
vivajane <obuekwekosi@gmail.com>
2022-09-08 16:41:49 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+nclude <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ *     */
+int main(void)                                    {
+	        printf("with proper grammar, but the outcome is a piece of art,\n");
+		        return (0);
+}
