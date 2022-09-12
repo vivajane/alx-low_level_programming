a6816b216d53a201e9bb98f0824748ff6ae4f256
vivajane <obuekwekosi@gmail.com>
2022-09-12 14:26:13 -0700

    C low level language: consditionals

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print single num separated by comma
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
