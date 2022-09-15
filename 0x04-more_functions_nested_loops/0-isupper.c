0108404868dff1242dd0431240475bbdfdc64cdb
vivajane <obuekwekosi@gmail.com>
2022-09-15 15:05:58 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,11 @@
  * Return: gives 1 or 0
  */
 int _isupper(int c)
-{
-        if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        return (0);
+{                     
+     if (c >= 'A' && c <= 'Z')
+     {
+	   return (1);
+     
+     }  
+     return (0);
+}
