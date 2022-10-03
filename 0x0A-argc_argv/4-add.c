8e4d08b6d62d2c4c098d79d54b5263d55347744f
vivajane <obuekwekosi@gmail.com>
2022-10-03 10:54:37 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,6 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  *         Otherwise - 0.
  */
-
 int main(int argc, char *argv[])
 {
 int num, digit, sum = 0;
@@ -18,7 +17,7 @@ for (num = 1; num < argc; num++)
 {
 for (digit = 0; argv[num][digit]; digit++)
 {
-if (argv[num][digit] < '0' || argv[num][digit] > '9'
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
 {
 printf("Error\n");
 return (1);
