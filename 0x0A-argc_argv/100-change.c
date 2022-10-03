75ccd84fecaf46c3b36442e047be42e0ad5ddd48
vivajane <obuekwekosi@gmail.com>
2022-10-03 12:41:55 -0700

    advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,21 +13,18 @@
 int main(int argc, char *argv[])
 {
 int cents, coins = 0;
-
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
 cents = atoi(argv[1]);
-
 while (cents > 0)
 {
 coins++;
 if ((cents - 25) >= 0)
 {
-cents -= 25
+cents -= 25;
 continue;
 }
 if ((cents - 10) >= 0)
@@ -47,8 +44,6 @@ continue;
 }
 cents--;
 }
-
 printf("%d\n", coins);
-
 return (0);
 }
