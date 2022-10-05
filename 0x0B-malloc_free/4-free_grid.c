9e0a99ca7861f1e8204ce3d567fda1999f6b5b17
vivajane <obuekwekosi@gmail.com>
2022-10-05 06:29:25 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,22 +7,15 @@
  * @grid: grid to check
  * @height: height of grid
  */
-
 void free_grid(int **grid, int height)
 {
 int i;
-
 if (height <= 0)
 return;
-
 if (grid == NULL)
 return;
-
 for (i = 0; i < height; i++)
-{
-free(grid[i]);
+{										free(grid[i]);
 }
-
 free(grid);
-
 }
