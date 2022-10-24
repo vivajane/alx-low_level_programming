9fca9f2cc7cde3f68bc7c51a4ca8a97655f40287
vivajane <obuekwekosi@gmail.com>
2022-10-24 02:42:54 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,7 +7,6 @@
  *
  *  Return: number of characters in a string
  */
-
 unsigned int _strlen(const char *s)
 {
 unsigned int len = 0;
@@ -51,4 +50,33 @@ s[i] = str[i];
 return (s);
 }
 }
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: Pointer to the pointer of beginning of the list_t list
+ * @str: String to be added
+ *
+ * Return: New node added
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new_node = NULL, *current_node = NULL;
+new_node = (list_t *)malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = _strdup(str);
+new_node->len = _strlen(str);
+new_node->next = NULL;
+if (*head == NULL)
+*head = new_node;
+else
+{
+current_node = *head;
+while (current_node->next != NULL)
+current_node = current_node->next;
+current_node->next = new_node;
+}
+return (new_node);
 }
